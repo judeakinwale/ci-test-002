fc7f531b7f11ba5de45c606fcfec86469251f418
judeakinwale <judeakinwale@gmail.com>
2022-10-05 16:10:36 +0100

    update routes

--- routes.js
+++ routes.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.status(200).send("Hello World!")
+})
+
 app.get('/test', (req, res) => {
     res.status(200).send("Hello World!")
 })
+
 
 module.exports = app
