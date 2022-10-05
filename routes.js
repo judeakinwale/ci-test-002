6e261ccc4a260b7540748c1bf9c68d70378bf0d7
judeakinwale <judeakinwale@gmail.com>
2022-10-05 16:53:11 +0100

    Update homepage message

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(200).send("Welcome!")
+    res.status(200).send("Welcome to the application!")
 })
 
 app.get('/test', (req, res) => {
